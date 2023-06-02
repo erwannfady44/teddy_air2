05592abc62983b4d2302359784c79523fba55a60
efady <erwann.fady@gmail.com>
2023-06-02 17:53:08 +0200

    ajout du serveur web

--- main.py
+++ main.py
@@ -1,18 +1,44 @@
-from arm import Arm
-import time
-try:
-    arm = Arm(23)  # Utilisation du port GPIO 23 pour le moteur
+from flask import Flask, request
+from flask_cors import CORS
+from models.arm import Arm
+from models.servo import Servo
+from models.motor import Motor
 
-    # Demarre la rotation continue du moteur
+app = Flask(__name__)
+CORS(app)
+
+speed = 0
+direction = 0
+sens = 0
+
+motor = Motor(2)
+arm = Arm(5)
+servo = Servo(4)
+@app.route('/drive', methods=['POST'])
+def drive():
+    #Récupération des données envoyées
+    data = request.get_json()
+    #Si on doit Reculer
+    if int(data.get('sens')) == -1:
+        #Fait reculer à la bonne vitesse
+        motor.backward(data.get('speed'))
+    #Si on doit avancer
+    elif int(data.get('sens')) == 1:
+        #Fait avancer à la bonne vitesse
+        motor.backward(data.get('speed'))
+    #Sinon
+    else:
+        #Arrêt du moteur
+        motor.stop()
+
+    #Si on ne va pas tout droit
+    if int(data.get('direction')) != 90:
+        #Fait tourner le servo moteur au bon angle
+        Servo.set_angle(data.get('direction'))
+
+    return "drive"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=False)
     arm.start()
-
-    a=Arm(5)
-    a.start()
-    # Laisse le moteur tourner pendant 5 secondes
-    time.sleep(5)
-
-    # Arrete la rotation
-    arm.stop()
-    time
-except KeyboardInterrupt:
-    arm.cleanup()
